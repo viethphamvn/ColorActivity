124aec22e2e14b7c3908b2c86a979d513b47dafb
Viet Pham <tug36669@temple.edu>
2019-09-25 10:48:31 -0400

    Initialized spinner and set its adapter to ColorAdapter

--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.coloractivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] colors = {"CYAN","BLUE","RED"};
+
+        Spinner colorSpinner = findViewById(R.id.colorSpinner);
+        ColorAdapter myAdapter = new ColorAdapter(this, colors.length, colors);
+        colorSpinner.setAdapter(myAdapter);
     }
 }
