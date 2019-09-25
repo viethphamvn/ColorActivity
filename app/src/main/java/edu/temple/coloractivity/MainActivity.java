c77b181716205bf9ac3dd2a360467868f8756a4a
Viet Pham <tug36669@temple.edu>
2019-09-25 10:59:39 -0400

    added layout variable to change activity background and added OnSelectedItemListener

--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -1,10 +1,15 @@
 package edu.temple.coloractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
         Spinner colorSpinner = findViewById(R.id.colorSpinner);
         ColorAdapter myAdapter = new ColorAdapter(this, colors.length, colors);
         colorSpinner.setAdapter(myAdapter);
+
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ConstraintLayout myLayout = findViewById(R.id.activityLayout);
+                TextView textView = (TextView) view;
+                myLayout.setBackgroundColor(Color.parseColor(textView.getText().toString()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
